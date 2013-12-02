ea35691801773cd6e8e08d1877aee83c9b51f17a
trauber <rich.traube@gmail.com>
2013-12-02 09:17:47 -0500

    Update config.js

--- config.js
+++ config.js
@@ -1,11 +1,10 @@
 var config = {
-  title: 'Grite',
+  title: 'trauber\'s grite',
   navbar: [ { text: 'Fork me on Github',
               link: 'https://github.com/sankha93/grite' },
             { text: 'Documentation',
               link: 'https://github.com/sankha93/grite/wiki/Documentation'} ],
-  posts: [ '5711471',
-           '5711492'],
+  posts: [ '7219242'],
   atOnce: 5,
   footerText: "Grite has been made with love by Sankha. Follow him on Twitter at <a href=\"https://twitter.com/sankha93\">@sankha93</a>."
 };
